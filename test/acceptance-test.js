9eddfbdb75d0786ab6760ca7c9b3e4d1c264ef45
kenleytomlin <kenleytomlin@gmail.com>
2013-11-18 13:52:38 +0000

    Split tests into specs and acceptance

---
+++ test/acceptance-test.js
@@ -0,0 +1,25 @@
+var net = require('net');
+var url = require('url');
+var assert = require('assert');
+var request = require('superagent');
+var nock = require('nock');
+var should = require('should');
+
+require('../')(request);
+
+describe('acceptance - superagent.Request#proxy()', function () {
+  it('should make a request to the http endpoint', function (done) {
+    request.get('http://yahoo.co.uk').proxy('http://localhost:3128').end(function (res) {
+      should.exist(res)
+      // res.body.ip.should.exist
+      done()
+    })
+  });
+
+  it('should make a request to the https endpoint', function (done) {
+    this.timeout(0);
+    request.get('https://yahoo.com').proxy('http://localhost:3128').end(function (res) {
+      should.exist(res)
+    })
+  })
+})
